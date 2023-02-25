f4514102412d1d534b3a8ebe95f52413d74afaf9
jihyun Yun <jihyun9842@gmail.com>
2023-02-25 11:08:24 +0900

    컴포넌트 분리하기2

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
 import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [id, setId] = useState("");
-  const [pw, setPw] = useState("");
+  const [users, setUsers] = useState([
+    { id: 1, age: 30, name: "송중기" },
+    { id: 1, age: 30, name: "송강" },
+    { id: 1, age: 30, name: "김유정" },
+    { id: 1, age: 30, name: "구교환" },
+  ]);
 
-  const onclickId = (event) => setId(event.target.value);
-  const onclickPw = (event) => setPw(event.target.value);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState('');
+
+  const nameChange = (event) => {
+    setName(event.target.value);
+    setName('')
+  };
+
+  const ageChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  const clickBtn = (event) => {
+    const obj = {
+      id: users.length +1,
+      age,
+      name,
+    }
+    setUsers([...users, obj])
+  }
   return (
     <div>
       <div>
-        아이디 : <input type="text" value={id} onChange={onclickId} />
+        이름 : &nbsp;<input type="text" value={name} onChange={nameChange} /> <br />
+        나이 : &nbsp;<input type="text" value={age} onChange={ageChange} />
+        <button onClick={clickBtn}>추가</button>
       </div>
-      <div>
-        비밀번호 : <input type="password" value={pw} onChange={onclickPw} />
+
+      <div className="app-style">
+        {users.map((i) => (
+          <div key={i.id} className="component-style">
+            {i.age} - {i.name}
+          </div>
+        ))}
       </div>
-      <button
-        onClick={() => {
-          alert(`회원님의 아이디는 ${id}이며, 비밀번호는 ${pw}입니다.`);
-          setId("");
-          setPw("");
-        }}
-      >
-        제출!
-      </button>
     </div>
   );
 }
